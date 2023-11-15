5db186f0e5df215b19f6407668e8933bd000f267
zayatsdan <36342659+zayatsdan@users.noreply.github.com>
2023-11-15 15:21:30 +0100

    Update PrintWordsApp.java

--- src/main/java/school/mjc/stage0/module3/task1/PrintWordsApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/PrintWordsApp.java
@@ -2,6 +2,6 @@ package school.mjc.stage0.module3.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        System.out.println("java is smth");
+        System.out.println("java, is, smth");
     }
 }
